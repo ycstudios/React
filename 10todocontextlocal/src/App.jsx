438540124f6a0a3126a3aabb4990d38c2f4f5dbc
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-03 07:21:05 +0530

    completed todo and learned about local storage context api

--- 10todocontextlocal/src/App.jsx
+++ 10todocontextlocal/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./context/TodoContext";
+import TodoForm from "./components/TodoFrom";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -20,9 +22,22 @@ function App() {
   }
 
   const toggleComplete=(id)=>{
-    setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id?{...prevTodo, completed:!prevTodo.completed}:prevTodo))
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id?{...prevTodo, completed:!prevTodo.completed}:prevTodo))
   }
- 
+  
+  useEffect(() => {
+   const todos=JSON.parse(localStorage.getItem('todos'))
+   if(todos && todos.length>0){
+    setTodos(todos)
+   }
+
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+    
+  },[todos])
+  
 
   return (
 <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
@@ -31,9 +46,15 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id} >
+                          <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
