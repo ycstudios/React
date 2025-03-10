b5d4db4e1c3d3ffe610a88f6359d97e819317563
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-10 18:16:43 +0530

    revist props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Card from './components/Card'
 import './App.css'
 
 function App() {
-  let myObj={
-    username:'yash',
-    age:21
-  }
 
-  let newArr=[1,2,3]
+
 
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
-    <Card username='chai aur code' someObj={myObj}/>
+    <h1 className='bg-green-400 p-3 rounded-xl'>Tailwind Test</h1>
+    <Card username="Yash" />
+    <Card  username='KDM' />
     </>
   )
 }
